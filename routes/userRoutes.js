af6874956270d8a2bbf01f01a812bdd74d44269b
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-06-04 21:37:37 +0530

    Connect To User Routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
 
+router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.get("/profile", getUserProfile);
+router.put("/profile", updateUserProfile);
 
 export default router;
